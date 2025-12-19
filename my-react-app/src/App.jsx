f7c3556212d53b6fc2b79d9119207bb11f5ad43d
2004khushi <goyalkhushi1004@gmail.com>
2025-12-20 00:39:48 +0530

    searchBar created

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,20 +1,27 @@
+import Search from './components/Search'
+import {useState} from 'react'
+
+const App = () => {
+
+    const[searchTerm, setSearchTerm] = useState('')
 
 
-function App() {
-  return (
-    <main>
-      <div className="pattern"/>
-      <div className = "wrapper">
-          <header>
-              <img src = "./hero.png" alt="Hero-Banner" />
-              <h1>
-                  Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle
-              </h1>
-          </header>
+    return (
+        <main>
+            <div className="pattern" />
 
-      </div>
-    </main>
-  )
+            <div className="wrapper">
+                <header>
+                    <img src="./hero.png" alt="Hero-Banner" />
+                    <h1>
+                        Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle
+                    </h1>
+                </header>
+
+                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+            </div>
+        </main>
+    )
 }
 
 export default App
