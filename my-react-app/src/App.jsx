4e04e3c6e16ac027896338445bcb9c77c417a09c
2004khushi <goyalkhushi1004@gmail.com>
2025-12-20 08:33:07 +0530

    API FETCHED

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,59 @@
 import Search from './components/Search'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
+
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+    Method: 'GET', //since we're going to get the data
+    headers:{
+        accept: 'application/json', //this is the format in which we want their app's data
+        Authorization: 'Bearer ' + API_KEY,     //this helps in telling the the other side that who's trying to make access (so to tell it's us we send our token key
+    }
+}
 
 const App = () => {
 
-    const[searchTerm, setSearchTerm] = useState('')
+    const[searchTerm, setSearchTerm] = useState('');
+    const[errorMessage, setErrorMessage] = useState('');
+    const [movieList, setMovieList] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+
+    const fetchMovies = async () => {
+        setIsLoading(true);
+        setErrorMessage('');
+
+        try{
+            const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+            const response = await fetch(endpoint, API_OPTIONS); //we getting endpoint by fetch() in react while passing basic api details for authentication+authorization
+
+            if(!response.ok){
+                throw new Error('Could not find movies from API');
+            }
+
+            const data = await response.json();
+            console.log(data)
+
+            if(data.Response === 'False'){
+                setErrorMessage(data.Error || 'Failed to fetch movies');
+                setMovieList([]);
+                return;
+            }
+
+            setMovieList(data.results || []);
+            
+        }catch(e){
+            console.log(`Error: ${e}`);
+            setErrorMessage('Error fetching movies. Please try again later... ');
+        }finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchMovies();
+    },[])
 
 
     return (
@@ -16,9 +66,20 @@ const App = () => {
                     <h1>
                         Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle
                     </h1>
+
+                    <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
                 </header>
 
-                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+                <section className='all-movies'>
+                    <h2> All Movies</h2>
+
+                    {isLoading ? (<p className='text-white'> Loading ...</p>) : errorMessage? (<p className='text-red-900'>{errorMessage}</p>) : <ul>
+                        {movieList.map((movie) => (
+                            <p key={movie.id} className='text-white'>{movie.title}</p>
+                        ))}
+                    </ul>}
+                </section>
+
             </div>
         </main>
     )
